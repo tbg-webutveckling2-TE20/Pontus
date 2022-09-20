eeb74de8c0c3e76f178238aadd1c701f202ca702
Pontus <pontus.lindberg@lycksele.se>
2022-09-20 13:48:59 +0200

    Projekt 1 - Bigfoot

---
+++ projekt/P1 - Bigfoot/assets/js/bigfoot.js
@@ -0,0 +1,7 @@
+function loadBigFoot() {
+    document.getElementById('bigFoot').src = 'assets/images/bigfoot.png'
+}
+
+function moveBigFoot() {
+    var picture = docuemnt.getElementById('bigFoot')
+}
